43ec4968cadbf3beb37434e4ea4c239e3a9ba181
marcellorvalle <marcellorvalle@gmail.com>
2020-01-23 16:00:00 -0300

    Update Professional.java

--- src/main/java/com/marcellorvalle/scheduler/entity/Professional.java
+++ src/main/java/com/marcellorvalle/scheduler/entity/Professional.java
@@ -1,7 +1,14 @@
 package com.marcellorvalle.scheduler.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 
 @Entity
+@Data
 public class Professional extends Person {
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Schedule schedule;
 }
